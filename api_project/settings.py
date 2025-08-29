86c6ad22e204a626a6115daf94750ea10c32cf85
therealjhay <johnsonoca@gmail.com>
2025-08-29 16:11:09 +0100

    product and Category models to the admin site.

--- api_project/settings.py
+++ api_project/settings.py
@@ -128,10 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Django REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication', # <-- Make sure this is present
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
@@ -140,5 +139,5 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10 # Set the number of items per page
+    'PAGE_SIZE': 10
 }
